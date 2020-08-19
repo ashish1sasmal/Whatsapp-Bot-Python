51f480731977a8c470b77158703403e7109f11f9
ashish1sasmal <ashishsasmal1@gmail.com>
2020-08-19 17:45:30 +0530

    first

--- app/views.py
+++ app/views.py
@@ -8,7 +8,23 @@ import json
 from django.views.generic import View
 from .mixin import HttpResponseMixin
 
+from twilio.rest import Client
+import os
+
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+
+def send(msg):
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+          from_='whatsapp:+14155238886',
+          body=msg,
+          to='whatsapp:+919911698098'
+                              )
+    print("send!")
+
 class Message(HttpResponseMixin,View):
     def post(self,request,*args,**kwargs):
         message = "Hello! Ashish"
+        send(request.POST('Body'))
         return HttpResponse(message)
